384e54253e8bf2e2b6c33f9c1de2ed2a08671c27
ducsco <vetcom63@gmail.com>
2021-11-02 22:21:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Booscoz - Shipper</title>
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#"><h1 style="color:blue">Booscoz</h1></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+      </ul>
+      <form class="d-flex">
+	    <a class="btn btn-outline-success" href="order">ĐẶT GIAO HÀNG NGAY</a>   
+        <a class="btn btn-outline-success" href="login">ĐĂNG NHẬP/ĐĂNG KÝ</a>
+      </form>
+    </div>
+  </div>
+</nav>
+<style>
+.dv-header-promo {
+    position: relative;
+    padding: 71px 30px;
+    background: #F4F4F4;
+}
+.dv-card-list__card {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    max-width: 352px;
+    width: 100%;
+    margin-right: 32px;
+    background-color: #ffffff;
+    padding: 48px 24px;
+    border-radius: 111px;
+}
+.layout__content-header {
+    max-width: 736px;
+    margin: 0 auto;
+    text-align: center;
+    font-size: 48px;
+    line-height: 60px;
+    font-weight: bold;
+    padding-bottom: 56px;
+}
+.dv-card-list__title {
+    margin-bottom: 8px;
+    font-size: 32px;
+    line-height: 44px;
+    font-weight: bold;
+    text-align: center;
+}
+.dv-card-list__description {
+    font-size: 16px;
+    line-height: 26px;
+    text-align: center;
+}
+.LandingMiniFormGlobal_Root_S-64A {
+    width: 100%;
+    max-width: 440px;
+    padding: 24px;
+    background: #ffffff;
+    border-radius: 16px;
+}
+.dv-small-card-list__items {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: -64px;
+}
+.dv-small-card-list__item {
+    width: 33.3%;
+    padding: 64px 64px 0 0;
+    list-style: none;
+    text-align: center;
+}
+</style>
+<div class="dv-header-promo">
+	<div class="container">
+		<div class="row">
+		<div class="col-sm-6">
+		<div class="nenxanh"><img src="https://borzodelivery.com/img/global/index/walkman.svg" alt="promo-image"></div>
+		</div>
+		<div class="col-sm-6">
+		<div class="nenxanh">
+			<h1 class="dv-header-promo__title"> <span style="font-size: 36px">Ứng dụng giao hàng<br> Siêu tốc - Siêu tiết kiệm</span> </h1>
+			<div class="dv-header-promo__subtitle">Giao hàng trong ngày nội thành TP.HCM, Hà Nội và Đà Nẵng</div></br>
+			<div class="LandingMiniFormGlobal_Root_S-64A">
+			<form>
+			  <div class="row mb-3">
+				<div class="col-sm">
+				  <input type="text" class="form-control" id="inputEmail3" placeholder="Địa chỉ nhận hàng">
+				</div>
+			  </div>
+			  <div class="row mb-3">
+				<div class="col-sm">
+				  <input type="password" class="form-control" id="inputPassword3" placeholder="Địa chỉ giao hàng">
+				</div>
+			  </div>
+			  <fieldset class="row mb-3">
+				<div class="col-sm">
+				  <div class="form-check">
+					<input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+					<label class="form-check-label" for="gridRadios1">
+					  Xe máy
+					</label>
+				  </div>
+				  <div class="form-check">
+					<input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+					<label class="form-check-label" for="gridRadios2">
+					  Xe tải
+					</label>
+				  </div>
+				</div>
+			  </fieldset>
+			  <button type="submit" class="btn btn-primary">ĐẶT GIAO NGAY</button>
+			</form>
+			</div>
+		</div>
+		</div>
+		</div>
+	</div>
+</div>
+<div class="dv-header-promo">
+	<div class="layout__content-header"> BOOSCOZ giao hàng siêu tốc </div>
+	<div class="dv-card-list" style="display: flex; justify-content: center;"> 
+		<div class="dv-card-list__card dv-card-list__card--vn"> 
+			<div class="dv-card-list__image-wrapper"> <img class="dv-card-list__image" src="https://borzodelivery.com/img/global/index/icons/delivery-tools/wallet.svg"> </div> 
+			<div class="dv-card-list__title"> Tiết kiệm tối đa </div> <div class="dv-card-list__description "> <b>TIẾT KIỆM</b><br> Giao hàng nội thành TP.HCM, Hà Nội và Đà Nẵng<br> </div> 
+		</div> 
+		<div class="dv-card-list__card dv-card-list__card--vn"> 
+			<div class="dv-card-list__image-wrapper"> <img class="dv-card-list__image" src="https://borzodelivery.com/img/global/index/icons/delivery-tools/clock.svg"> </div> 
+			<div class="dv-card-list__title"> Hẹn giờ linh hoạt </div> 
+			<div class="dv-card-list__description "> <b>GIAO HÀNG HẸN GIỜ</b><br> Lên đơn trước và ngày giờ giao hàng mong muốn </div> 
+		</div> 
+		<div class="dv-card-list__card dv-card-list__card--vn"> 
+			<div class="dv-card-list__image-wrapper"> <img class="dv-card-list__image" src="https://borzodelivery.com/img/global/index/icons/delivery-tools/tools.svg"> </div> 
+			<div class="dv-card-list__title"> Giao hàng tiện lợi </div> 
+			<div class="dv-card-list__description "> <b>TỰ ĐỘNG HÓA</b><br> Kết nối API giúp tự động hoá quy trình giao nhận </div> 
+		</div> 
+	</div>
+	<section class="layout__content-section"> 
+		<div class="layout__content-wrapper layout__content-wrapper--padding"> 
+			<div class="layout__content-header" style="padding: 72px 0;"> Tại sao bạn nên chọn Booscoz? </div> 
+			<div class="dv-small-card-list" style="background:#ffffff; padding: 48px 24px;border-radius: 51px"> 
+			<ul class="dv-small-card-list__items"> 
+				<li class="dv-small-card-list__item"> 
+					<img src="https://borzodelivery.com/img/global/index/icons/why-choose-us/wizard.svg">
+					<div class="dv-small-card-list__text">Với đội ngũ tài xế chuyên biệt và hệ thống quản lý linh hoạt, Booscoz sẽ đảm bảo đơn hàng được giao nhận như ý</div> 
+				</li> 
+				<li class="dv-small-card-list__item"> 
+					<img src="https://borzodelivery.com/img/global/index/icons/why-choose-us/calc.svg">
+					<div class="dv-small-card-list__text">Hệ thống tự động tối ưu hoá tuyến đường giúp khách hàng tiết kiệm chi phí hiệu quả</div> 
+				</li> 
+				<li class="dv-small-card-list__item"> 
+					<img src="https://borzodelivery.com/img/global/index/icons/why-choose-us/rate.svg">
+					<div class="dv-small-card-list__text">Đội ngũ tài xế luôn trang bị dụng cụ giao hàng phù hợp, giúp bảo quản sản phẩm tốt nhất khi đến tay khách hàng</div> 
+				</li> 
+				<li class="dv-small-card-list__item"> 
+					<img src="https://borzodelivery.com/img/global/index/icons/why-choose-us/api.svg">
+					<div class="dv-small-card-list__text">Tiết kiệm thời gian với tính năng lên đơn hàng loạt - tự động hóa quy trình giao nhận</div> 
+				</li> 
+				<li class="dv-small-card-list__item"> 
+					<img src="https://borzodelivery.com/img/global/index/icons/why-choose-us/sms.svg">
+					<div class="dv-small-card-list__text">Theo dõi đơn hàng mọi lúc, mọi nơi: Toàn bộ thông tin trạng thái đơn hàng sẽ được cập nhật cho người nhận và người gửi qua SMS</div> 
+				</li> 
+				<li class="dv-small-card-list__item"> 
+					<img src="	https://borzodelivery.com/img/global/index/icons/why-choose-us/rocket.svg">
+					<div class="dv-small-card-list__text">Dịch vụ giao hàng linh hoạt: giao hàng hẹn giờ, giao hàng siêu tốc và ứng tiền lên đến 3.000.000đ</div> 
+				</li> 
+			</ul>
+			<div class="layout__content-header" style="padding: 72px 0;"> Phương thức thanh toán </div>
+			<div class="dv-card-list" style="display: flex; justify-content: center;"> 
+				<div class="dv-card-list__card dv-card-list__card--vn"> 
+					<div class="dv-card-list__image-wrapper"> <img class="dv-card-list__image" src="https://borzodelivery.com/img/global/index/icons/payment/cash.svg"> </div> 
+					<div class="dv-card-list__title"> Tiền mặt </div> 
+				</div> 
+				<div class="dv-card-list__card dv-card-list__card--vn"> 
+					<div class="dv-card-list__image-wrapper"> <img class="dv-card-list__image" src="https://borzodelivery.com/img/global/index/icons/payment/wallet.svg"> </div> 
+					<div class="dv-card-list__title"> Nạp tiền vào ví bằng Ví điện tử hoặc Thẻ ngân hàng </div> 	
+				</div> 
+			</div>
+			</div> 
+		</div> 
+	</section>
+</div>
+<section class="layout__content-section dv-index-info__layout" style="background: #2D2928;"> 
+	<div style="padding: 43px 0;"> </div>
+		<div style="color: #ffffff;margin: 0 auto;width: 705px; max-width: 736px;
+    margin: 0 auto;
+    text-align: center;
+    font-size: 48px;
+    line-height: 60px;
+    font-weight: bold;
+    padding-bottom: 56px;"> Hãy thử trải nghiệm dịch vụ </div> 
+</body>
+<footer class="footer text-center">	
+	<div id="footer" class="mtop10">
+		<a><span class="nav-label" style="color:#ffffff">Dev by MaiLike.xyz | Điều khoản & Chính Sách</span></a></br>
+	</div>
+	<div style="padding: 12px 0;"> </div>
+</footer>
+</html>
